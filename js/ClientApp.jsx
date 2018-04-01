7e4ea70cf085a6237c8c09bf6eeff1f087956b2d
David Yonwook Chung <yon_gospel@hotmail.com>
2018-03-31 22:03:14 -0700

    [react] estalishes HashRouter

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
+import { HashRouter, Route } from "react-router-dom";
+
+import Landing from "./Landing";
+import Search from "./Search";
 
 const App = () => (
-  <div className="app">
-    <div className="landing">
-      <h1>svideo</h1>
-      <input type="text" placeholder="Search" />
-      <a>or Browse All</a>
-    </div>
-  </div>
+  <HashRouter>
+    <div className="app">
+      <Route exact path="/" component={Landing} />
+      <Route path="/search" component={Search} />
+    </div>{" "}
+  </HashRouter>
 );
 
 render(<App />, document.getElementById("app"));
